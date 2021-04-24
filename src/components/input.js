905dcb40e0fd0d76004b452241cb69d7d21bbfa5
ksmlgl <kasimaligul@gmail.com>
2021-04-24 18:24:40 +0300

    input component rev v2

--- src/components/input.js
+++ src/components/input.js
@@ -5,8 +5,8 @@ const Input = (props) => {
     const className = error ?  "form-control is-invalid" : "form-control" ;
     return (
         <div className="form-group">
-            <label>Username</label>
-            <input className={className} name="{name}" onChange={onChange}/>
+            <label>{label}</label>
+            <input className={className} name={name} onChange={onChange}/>
             <div className="invalid-feedback">
                 {error}
             </div>
--- src/pages/UserSignUpPage.js
+++ src/pages/UserSignUpPage.js
@@ -70,21 +70,9 @@ class UserSignUpPage extends React.Component {
             <div className="container">
                 <form>
                     <h1 className="text-center">Sign Up</h1>
-                    <Input name="username" label="UserName" error={username} onChange={this.onChange}/>
-                    {/*<div className="form-group">
-                        <label>Username</label>
-                        <input className={username ? "form-control is-invalid" : "form-control"} name="username" onChange={this.onChange}/>
-                        <div className="invalid-feedback">
-                            {username}
-                        </div>
-                    </div>*/}
-                    <div className="form-group">
-                        <label>Display Name</label>
-                        <input className={displayName ? "form-control is-invalid" : "form-control"} name="displayName" onChange={this.onChange}/>
-                        <div className="invalid-feedback">
-                            {displayName}
-                        </div>
-                    </div>
+                    <Input name="username" label="User Name" error={username} onChange={this.onChange}/>
+                    <Input name="displayName" label="Display Name" error={displayName} onChange={this.onChange}/>
+
                     <div className="form-group">
                         <label>Password</label>
                         <input className="form-control" name="password" type="password" onChange={this.onChange}/>
