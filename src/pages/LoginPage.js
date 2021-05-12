f0470ceb692cb46e8725314591ff409cd3e4d9ee
ksmlgl <kasimaligul@gmail.com>
2021-05-12 23:26:12 +0300

    login page security messages

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -7,35 +7,48 @@ class LoginPage extends React.Component {
 
 state ={
     username:null,
-    password:null
+    password:null,
+    error:null
 }
 
 onChange = event => {
     const {name, value}=event.target;
     this.setState({
-        [name]:value
+        [name]:value,
+        error:null
     })
 }
 
-onClickLogin = event => {
+onClickLogin = async event => {
     event.preventDefault();
     const {username, password} = this.state;
     const creds = {
         username,password
     }
-    login(creds);
+    try{
+        await login(creds);
+    }catch(apiError){
+        this.setState({error:apiError.response.data.message});
+    }
 }
 
     render() {
         const {t} = this.props;
+        const{username, password, error} = this.state;
+        const buttonEnabled = username && password && !error;
         return (
             <div className="container">
                 <form>
                     <h1 className="text-center">{t('Login')}</h1>
                     <Input label={t("Username")} name="username" onChange={this.onChange}></Input>
                     <Input label={t("Password")} name="password" type="password" onChange={this.onChange}></Input>
+                    {this.state.error && 
+                    <div className="alert alert-danger">
+                       {this.state.error}
+                    </div>
+                    }
                     <div className="text-center">
-                    <button onClick={this.onClickLogin}>{t('Login')}</button>
+                    <button onClick={this.onClickLogin} disabled={!buttonEnabled}>{t('Login')}</button>
                     </div>
                 </form>
                 
