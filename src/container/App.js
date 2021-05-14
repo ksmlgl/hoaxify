044029c3071673ad2a8569848319ea026ec61324
ksmlgl <kasimaligul@gmail.com>
2021-05-14 13:44:53 +0300

    user and home pages

--- src/container/App.js
+++ src/container/App.js
@@ -4,17 +4,15 @@ import ApiProgress from '../shared/ApiProgress'
 import UserSignUpPage from '../pages/UserSignUpPage'
 import LoginPage from '../pages/LoginPage'
 import LanguageSelector from '../components/LanguageSelector'
+import HomePage from '../pages/HomePage'
+import UserPage from '../pages/UserPage'
+
 
 function App() {
   return (
   
       <div className="row">
-        <div className="col">
-            <UserSignUpPage />
-        </div>
-        <div className="col">
-            <LoginPage />
-        </div>
+        <UserPage/>
       <LanguageSelector/>
       </div>
 
