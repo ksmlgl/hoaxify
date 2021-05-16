4a14c444faf395d4c31be4c46cef36c4fd9a91f6
ksmlgl <kasimaligul@gmail.com>
2021-05-16 13:54:50 +0300

    lifting state up bad practice

--- src/container/App.js
+++ src/container/App.js
@@ -37,7 +37,9 @@ class App extends React.Component {
               return <LoginPage {...props} onLoginSuccess={this.onLoginSuccess} />
             }} />)}
             <Route path="/signup" component={UserSignUpPage} />
-            <Route path="/user/:username" component={UserPage} />
+            <Route path="/user/:username" component={props => {
+              return <UserPage {...this.props} username={username} />
+            }} />
             <Redirect to="/" />
           </Switch>
 
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProfileCard from '../components/ProfileCard'
 
-const UserPage = () => {
+const UserPage = (props) => {
+    
     return (
         <div className="container">
-            User Page
+            <ProfileCard username={props.username}/>
         </div>
     );
 };
