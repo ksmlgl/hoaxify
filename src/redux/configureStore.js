090fa4a97748651450f72b6066c1fb10e38165a2
ksmlgl <kasimaligul@gmail.com>
2021-05-19 00:40:33 +0300

    save redux store to local storage

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -7,11 +7,36 @@ const loggedInState = {
     displayName: 'display1',
     image: null,
     password: 'P4ssword'
-  
-  }
 
-  const configureStore = () => {
-      return createStore(authReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-  }
+}
 
-  export default configureStore;
+const configureStore = () => {
+
+    const hoaxAuth = localStorage.getItem('hoax-auth');
+
+    let stateInLocalStorage = {
+        isLoggedIn: false,
+        username: undefined,
+        displayName: undefined,
+        image: null,
+        password: undefined
+    }
+    if (hoaxAuth) {
+        try {
+            stateInLocalStorage = JSON.parse(hoaxAuth);
+        } catch (error) {
+
+        }
+
+    }
+
+    const store = createStore(authReducer, stateInLocalStorage, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+    store.subscribe(() => {
+        localStorage.setItem('hoax-auth', JSON.stringify(store.getState()));
+    })
+
+    return store;
+}
+
+export default configureStore;
