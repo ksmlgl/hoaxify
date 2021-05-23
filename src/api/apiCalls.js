9c338eea279c891b61525dc322e209565ea0375e
ksmlgl <kasimaligul@gmail.com>
2021-05-23 13:25:06 +0300

    user page getUser call

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -26,3 +26,7 @@ export const setAuthorizationHeader = ({username, password, isLoggedIn}) => {
     }
     
 }
+
+export const getUser = (username) => {
+    return axios.get(`/api/1.0/users/${username}`);
+}
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfileCard from '../components/ProfileCard'
+import { getUser } from '../api/apiCalls'
 
 const UserPage = (props) => {
-    
+
+    const [user, setUser] = useState();
+
+    const { username } = props.match.params;
+    useEffect(() => {
+        const loadUser = async () => {
+            try {
+                const response = await getUser(username);
+                setUser(response.data);
+            } catch (error) {
+
+            }
+        }
+        loadUser();
+    }, [username]);
+
+
+
     return (
         <div className="container">
             <ProfileCard />
