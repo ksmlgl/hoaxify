3fea922a3482ec379223d762b9143206945d7348
ksmlgl <kasimaligul@gmail.com>
2021-05-24 23:57:49 +0300

    file cache and store file static

---
+++ src/main/java/com/hoxify/ws/configuration/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.hoxify.ws.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author KSM
+ * @since 24.05.2021 23:34
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**").addResourceLocations("file:./picture-storage/")
+				.setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+	}
+}
