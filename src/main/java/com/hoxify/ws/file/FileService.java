9e4499088d5eb487a37cd841aa6785a973331f58
ksmlgl <kasimaligul@gmail.com>
2021-05-25 23:07:00 +0300

    remove old picture files

--- src/main/java/com/hoxify/ws/file/FileService.java
+++ src/main/java/com/hoxify/ws/file/FileService.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.UUID;
 
@@ -24,7 +26,7 @@ public class FileService {
 
 	public String writeBase64EncodedStringToFile(String image) throws IOException {
 		String fileName = generateRandomName();
-		File target = new File(appConfiguration.getUploadPath() + fileName);
+		File target = new File(appConfiguration.getUploadPath() +"/"+ fileName);
 		OutputStream outputStream = new FileOutputStream(target);
 		byte[] base64encoded = Base64.getDecoder().decode(image);
 		outputStream.write(base64encoded);
@@ -34,5 +36,16 @@ public class FileService {
 
 	private String generateRandomName() {
 		return UUID.randomUUID().toString().replaceAll("-", "");
+	}
+
+	public void deleteFile(String oldImageName) {
+		if(oldImageName == null){
+			return;
+		}
+		try {
+			Files.deleteIfExists(Paths.get(appConfiguration.getUploadPath(), oldImageName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
