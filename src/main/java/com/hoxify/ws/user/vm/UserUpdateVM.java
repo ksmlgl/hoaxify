aecfe6c8fc7ecccf59a3816a18e65f9102f940d1
ksmlgl <kasimaligul@gmail.com>
2021-05-25 23:28:37 +0300

    validate update display name

--- src/main/java/com/hoxify/ws/user/vm/UserUpdateVM.java
+++ src/main/java/com/hoxify/ws/user/vm/UserUpdateVM.java
@@ -2,12 +2,19 @@ package com.hoxify.ws.user.vm;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * @author KSM
  * @since 23.05.2021 15:50
  */
 @Data
 public class UserUpdateVM {
+
+	@NotNull
+	@Size(min = 4, max=255)
 	private String displayName;
+
 	private String image;
 }
