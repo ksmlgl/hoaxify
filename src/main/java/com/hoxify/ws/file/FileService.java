de30aa2d95f9e45e5f84483c1334f75dd395702b
ksmlgl <kasimaligul@gmail.com>
2021-05-25 23:37:50 +0300

    tika file type detector library added

--- src/main/java/com/hoxify/ws/file/FileService.java
+++ src/main/java/com/hoxify/ws/file/FileService.java
@@ -1,6 +1,9 @@
 package com.hoxify.ws.file;
 
 import com.hoxify.ws.configuration.AppConfiguration;
+import org.apache.tika.Tika;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -21,14 +24,23 @@ import java.util.UUID;
 @Service
 public class FileService {
 
+	private static final Logger log = LoggerFactory.getLogger(FileService.class);
 	@Autowired
 	AppConfiguration appConfiguration;
 
 	public String writeBase64EncodedStringToFile(String image) throws IOException {
+
+		Tika tika = new Tika();
+
 		String fileName = generateRandomName();
 		File target = new File(appConfiguration.getUploadPath() +"/"+ fileName);
 		OutputStream outputStream = new FileOutputStream(target);
 		byte[] base64encoded = Base64.getDecoder().decode(image);
+
+		String fileType = tika.detect(base64encoded);
+
+		log.info(fileType);
+
 		outputStream.write(base64encoded);
 		outputStream.close();
 		return fileName;
