8e824a2b70cffdd4f5de6fe3932543ab463c0473
ksmlgl <kasimaligul@gmail.com>
2021-05-28 23:06:26 +0300

    load old hoaxes

--- src/components/HoaxFeed.js
+++ src/components/HoaxFeed.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getHoaxes } from '../api/apiCalls'
+import { getHoaxes, getOldHoaxes } from '../api/apiCalls'
 import { useTranslation } from 'react-i18next'
 import HoaxView from './HoaxView'
 import { useApiProgress } from '../shared/ApiProgress';
@@ -14,15 +14,39 @@ const HoaxFeed = () => {
     const [hoaxPage, setHoaxPage] = useState({ content: [], last: true, number: 0 });
     const { username } = useParams();
 
-    const path = username ? `/api/1.0/users/${username}/hoaxes?page=`: '/api/1.0/hoaxes?page=';
-    const pendingApiCall = useApiProgress('get',path);
-    useEffect(() => {
-        loadHoaxes();
-    }, []);
+    const path = username ? `/api/1.0/users/${username}/hoaxes?page=` : '/api/1.0/hoaxes?page=';
+    const initialHoaxLoadProgress = useApiProgress('get', path);
 
-    const loadHoaxes = async (page) => {
+    let lastHoaxId = 0;
+    if(hoaxPage.content.length>0){
+        const lastHoaxIndex = hoaxPage.content.length - 1;
+        lastHoaxId = hoaxPage.content[lastHoaxIndex].id;
+    }
+    
+    const loadOldHoaxesProgress = useApiProgress('get', '/api/1.0/hoaxes/'+lastHoaxId)
+    useEffect(() => {
+        const loadHoaxes = async (page) => {
+            try {
+                const response = await getHoaxes(username, page);
+                setHoaxPage(previousHoaxPage => ({
+                    ...response.data,
+                    content: [...previousHoaxPage.content, ...response.data.content]
+                }));
+            } catch (error) {
+
+            }
+        };
+        loadHoaxes();
+    }, [username]);
+
+
+
+    const loadOldHoaxes = async () => {
+
+
         try {
-            const response = await getHoaxes(username,page);
+            const response = await getOldHoaxes(lastHoaxId);
+
             setHoaxPage(previousHoaxPage => ({
                 ...response.data,
                 content: [...previousHoaxPage.content, ...response.data.content]
@@ -31,12 +55,12 @@ const HoaxFeed = () => {
 
         }
 
-    }
+    };
 
-    const { content, last, number } = hoaxPage;
+    const { content, last } = hoaxPage;
 
     if (content.length === 0) {
-        return <div className="alert alert-secondary text-center">{pendingApiCall ? <Spinner /> : t('There are no hoaxes')}</div>
+        return <div className="alert alert-secondary text-center">{initialHoaxLoadProgress ? <Spinner /> : t('There are no hoaxes')}</div>
     }
 
     return (
@@ -46,8 +70,8 @@ const HoaxFeed = () => {
             })
             }
             {!last && <div
-                className="alert alert-secondary text-center mt-1" style={{ cursor: pendingApiCall ? 'not-allowed':'pointer' }} onClick={pendingApiCall ? () => {} : () => loadHoaxes(number + 1)} >
-                {pendingApiCall ? <Spinner /> : t('Load old hoaxes')}</div>}
+                className="alert alert-secondary text-center mt-1" style={{ cursor: loadOldHoaxesProgress ? 'not-allowed' : 'pointer' }} onClick={loadOldHoaxesProgress ? () => { } : () => loadOldHoaxes()} >
+                {loadOldHoaxesProgress ? <Spinner /> : t('Load old hoaxes')}</div>}
         </div>
     );
 };
