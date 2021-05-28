6295c5df7659312810d0c1a7ab83d9d9c2ff579e
ksmlgl <kasimaligul@gmail.com>
2021-05-29 00:18:07 +0300

    new hoaxes user relative

--- src/components/HoaxFeed.js
+++ src/components/HoaxFeed.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getHoaxes, getOldHoaxes } from '../api/apiCalls'
+import { getHoaxes, getNewHoaxCount, getOldHoaxes } from '../api/apiCalls'
 import { useTranslation } from 'react-i18next'
 import HoaxView from './HoaxView'
 import { useApiProgress } from '../shared/ApiProgress';
@@ -9,21 +9,39 @@ import { useParams } from 'react-router-dom'
 
 const HoaxFeed = () => {
 
-
     const { t } = useTranslation();
     const [hoaxPage, setHoaxPage] = useState({ content: [], last: true, number: 0 });
+    const [newHoaxCount, setNewHoaxCount] = useState(0);
     const { username } = useParams();
 
     const path = username ? `/api/1.0/users/${username}/hoaxes?page=` : '/api/1.0/hoaxes?page=';
     const initialHoaxLoadProgress = useApiProgress('get', path);
 
     let lastHoaxId = 0;
-    if(hoaxPage.content.length>0){
+    let firstHoaxId = 0;
+    if (hoaxPage.content.length > 0) {
         const lastHoaxIndex = hoaxPage.content.length - 1;
         lastHoaxId = hoaxPage.content[lastHoaxIndex].id;
+        firstHoaxId = hoaxPage.content[0].id;
     }
-    const oldHoaxPath = username ? `/api/1.0/users/${username}/hoaxes/${lastHoaxId}`: '/api/1.0/hoaxes/'+lastHoaxId;
-    const loadOldHoaxesProgress = useApiProgress('get', oldHoaxPath, true)
+    const oldHoaxPath = username ? `/api/1.0/users/${username}/hoaxes/${lastHoaxId}` : '/api/1.0/hoaxes/' + lastHoaxId;
+    const loadOldHoaxesProgress = useApiProgress('get', oldHoaxPath, true);
+
+
+    useEffect(() => {
+        const getCount = async () => {
+            const response = await getNewHoaxCount(firstHoaxId, username);
+            setNewHoaxCount(response.data.count);
+        }
+        let looper = setInterval(() => {
+            getCount();
+        }, 1000);
+
+        return function cleanUp(){
+            clearInterval(looper);
+        }
+    }, [firstHoaxId, username]);
+
     useEffect(() => {
         const loadHoaxes = async (page) => {
             try {
@@ -44,7 +62,7 @@ const HoaxFeed = () => {
     const loadOldHoaxes = async () => {
 
         try {
-            const response = await getOldHoaxes(lastHoaxId,username);
+            const response = await getOldHoaxes(lastHoaxId, username);
 
             setHoaxPage(previousHoaxPage => ({
                 ...response.data,
@@ -59,11 +77,12 @@ const HoaxFeed = () => {
     const { content, last } = hoaxPage;
 
     if (content.length === 0) {
-        return <div className="alert alert-secondary text-center">{initialHoaxLoadProgress ? <Spinner /> : t('There are no hoaxes')}</div>
+        return <div className="alert alert-secondary text-center mb-1">{initialHoaxLoadProgress ? <Spinner /> : t('There are no hoaxes')}</div>
     }
 
     return (
         <div>
+            {newHoaxCount>0 &&  <div className="alert alert-secondary text-center">{t('There are new hoaxes')}</div>}
             {content.map(hoax => {
                 return <HoaxView key={hoax.id} hoax={hoax} />
             })
