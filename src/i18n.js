7d071bea0cdc0ec3664d95790f0b9b6bac1c35d7
ksmlgl <kasimaligul@gmail.com>
2021-05-29 00:05:56 +0300

    new hoaxes

--- src/i18n.js
+++ src/i18n.js
@@ -26,7 +26,8 @@ i18n.use(initReactI18next).init({
                 'Change Display Name':'Change Display Name',
                 'My Profile': 'My Profile',
                 'There are no hoaxes': 'There are no hoaxes',
-                'Load old hoaxes':'Load old hoaxes'
+                'Load old hoaxes':'Load old hoaxes',
+                'There are new hoaxes':'There are new hoaxes'
             }
         },
         tr: {
@@ -51,7 +52,8 @@ i18n.use(initReactI18next).init({
                 'Change Display Name':'Görünür isminizi değiştirin',
                 'My Profile': 'Hesabım',
                 'There are no hoaxes': 'Hoax bulunamadı',
-                'Load old hoaxes':'Eski hoaxları getir'
+                'Load old hoaxes':'Eski hoaxları getir',
+                'There are new hoaxes': 'Yeni hoaxlar var'
             }
         }
     },
