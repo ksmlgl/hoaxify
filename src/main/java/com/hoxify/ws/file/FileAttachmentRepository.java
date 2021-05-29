a7c910176280e9813d3890d0880584f71c52259b
ksmlgl <kasimaligul@gmail.com>
2021-05-29 14:13:03 +0300

    delete unused file scheduled

--- src/main/java/com/hoxify/ws/file/FileAttachmentRepository.java
+++ src/main/java/com/hoxify/ws/file/FileAttachmentRepository.java
@@ -2,9 +2,14 @@ package com.hoxify.ws.file;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author KSM
  * @since 29.05.2021 13:54
  */
 public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
+
+	List<FileAttachment> findByDateBeforeAndHoaxIsNull(Date date);
 }
