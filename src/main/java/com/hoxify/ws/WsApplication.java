b72cdba239411995a7627fbc8975829b8b98b8c4
ksmlgl <kasimaligul@gmail.com>
2021-06-13 14:36:23 +0300

    stored db

--- src/main/java/com/hoxify/ws/WsApplication.java
+++ src/main/java/com/hoxify/ws/WsApplication.java
@@ -22,18 +22,22 @@ public class WsApplication {
 	@Profile("dev")
 	CommandLineRunner createInitialUser(UserService userService, HoaxService hoaxService) {
 		return (args) -> {
-			for(int i = 1; i <= 25; i++){
-				User user = new User();
-				user.setUsername("user"+i);
-				user.setDisplayName("display"+i);
-				user.setPassword("P4ssword");
-				userService.save(user);
-				for(int j=1; j<=20; j++){
-					HoaxSubmitVM hoax = new HoaxSubmitVM();
-					hoax.setContent("hoax ("+j+") from user ("+i+")");
-					hoaxService.save(hoax, user);
-				}
+			for(int i = 1; i <= 25; i++) {
+				try {
+					userService.getByUsername("user" + i);
+				} catch (Exception e) {
+					User user = new User();
+					user.setUsername("user" + i);
+					user.setDisplayName("display" + i);
+					user.setPassword("P4ssword");
+					userService.save(user);
+					for (int j = 1; j <= 20; j++) {
+						HoaxSubmitVM hoax = new HoaxSubmitVM();
+						hoax.setContent("hoax (" + j + ") from user (" + i + ")");
+						hoaxService.save(hoax, user);
+					}
 
+				}
 			}
 
 
